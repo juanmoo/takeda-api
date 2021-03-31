0e28c35529e7285fd8245506396b62da5476672c
Juan M Ortiz <juanmoo@me.com>
2021-03-31 18:07:51 -0400

    create ner input

--- extract.py
+++ extract.py
@@ -1,12 +1,22 @@
 import utils
 import os
-from medtrialextractor import formatting
+from ya.dotdict import DotDict
 from tempfile import TemporaryDirectory
+from medtrialextractor import formatting
 
+def create_extraction(input_struct_path, model_dir, debug=False):
 
-def create_extraction(struct_path, model_dir):
+    if debug:
+        temp_dir = '/data/rsg/nlp/juanmoo1/projects/05_dev/workdir/tempdir'
+        root_dir = os.path.join(temp_dir, 'root')
+
+    else:
+        temp_dir = TemporaryDirectory()
+        root_dir = os.path.join(temp_dir.name, 'root')
 
     # Step 1: Create empty NER input
+    ner_input_file_path = os.path.join(root_dir, 'ner_input.txt')
+    formatting.make_empty_ner_bio(input_struct_path, ner_input_file_path)
 
     # Step 2: Make NER predictions
 
@@ -19,10 +29,11 @@ def create_extraction(struct_path, model_dir):
     # Step 6: Load RD predictions
 
     # Step 7: Creat tabular summary output
-    pass
 
 
 if __name__ == '__main__':
 
     input_struct_path = '/data/rsg/nlp/juanmoo1/projects/05_dev/workdir/collections_dir/Hello/structs/struct_base.json'
     model_dir = '/data/rsg/nlp/juanmoo1/projects/05_dev/workdir/models/8020/'
+
+    create_extraction(input_struct_path, model_dir, debug=True)
